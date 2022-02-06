33e6b2fbd69707062c97cd4028b0341f7ccace67
Aeonik Chaos <aeonik.chaos@gmail.com>
2022-02-06 05:04:35 -0500

    Added keys for each hands and each player.

--- src/clojure_card_games/core.clj
+++ src/clojure_card_games/core.clj
@@ -1,26 +1,32 @@
 (ns clojure-card-games.core
   (:gen-class)
-  (:require [clojure.math.combinatorics :as combo]))
+  (:require [clojure.math.combinatorics :as combo]
+            [clojure.pprint :as pp]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
 
-(def cards (list 9 10 :J :Q :K :A))
-(def suits (list :♥ :♠ :♦ :♣))
+(def cards (vector 9 10 :J :Q :K :A))
+(def suits (vector :♥ :♠ :♦ :♣))
 
 (print cards)
 (print suits)
 
-(print (combo/cartesian-product cards suits))
-(def karbosh-deck (mapcat (partial repeat 2)
-                          (combo/cartesian-product cards suits)))
-(print karbosh-deck)
+(pp/pprint (map vec (combo/cartesian-product cards suits)))
+(def karbosh-deck (map vec (mapcat (partial repeat 2)
+                                   (combo/cartesian-product cards suits))))
+(pp/pprint karbosh-deck)
 
 (def shuffled-deck (shuffle karbosh-deck))
-(print shuffled-deck)
+(pp/pprint shuffled-deck)
 
-(run! println (partition 6 shuffled-deck))
+(def hands (map vec (partition 8 shuffled-deck)))
+(pp/pprint hands)
 
-(print (map :K shuffled-deck))
+(def players [:player1 :player2 :player3 :player4 :player5 :player6])
+
+(pp/pprint (partition 8 shuffled-deck))
+(pp/pprint (zipmap players hands))
+(pp/pprint (zipmap players (mapv hash-map (repeat :hand) hands)))
