67dd0ebbd6b7e5f54b00a05b4cfc8e258111a349
aeonik <aeonik.chaos@gmail.com>
2022-05-09 14:07:14 -0400

    Cleaned up some code. Added some comments.

--- src/clojure_card_games/core.clj
+++ src/clojure_card_games/core.clj
@@ -11,22 +11,14 @@
 (def cards (vector 9 10 :J :Q :K :A))
 (def suits (vector :♥ :♠ :♦ :♣))
 
-(print cards)
-(print suits)
-
-(pp/pprint (map vec (combo/cartesian-product cards suits)))
 (def karbosh-deck (map vec (mapcat (partial repeat 2)
                                    (combo/cartesian-product cards suits))))
-(pp/pprint karbosh-deck)
 
 (def shuffled-deck (shuffle karbosh-deck))
-(pp/pprint shuffled-deck)
 
 (def hands (map vec (partition 8 shuffled-deck)))
-(pp/pprint hands)
 
 (def players [:player1, :player2, :player3, :player4, :player5, :player6])
-(pp/pprint players)
 
 (pp/pprint (partition 8 shuffled-deck))
 (pp/pprint (zipmap players hands))
@@ -35,17 +27,17 @@
 (pp/pprint game)
 (pp/pprint players)
 
-(def game (assoc {} :game game))
-(def game (assoc-in game [:game :player1 :team] 1))
-(def game (assoc-in game [:game :player2 :team] 2))
-(def game (assoc-in game [:game :player3 :team] 1))
-(def game (assoc-in game [:game :player4 :team] 2))
-(def game (assoc-in game [:game :player5 :team] 1))
-(def game (assoc-in game [:game :player6 :team] 2))
-(pp/pprint game)
+(comment
+  (def game (assoc {} :game game))
+  (def game (assoc-in game [:game :player1 :team] 1))
+  (def game (assoc-in game [:game :player2 :team] 2))
+  (def game (assoc-in game [:game :player3 :team] 1))
+  (def game (assoc-in game [:game :player4 :team] 2))
+  (def game (assoc-in game [:game :player5 :team] 1))
+  (def game (assoc-in game [:game :player6 :team] 2)))
 
 (def game-with-players
-  (reduce-kv (fn [data player team-no]
-               (assoc-in data [:game player :team] team-no))
+  (reduce-kv (fn [data player team-number]
+               (assoc-in data [:game player :team] team-number))
              {:game game}
-             {:player1 1, :player2 2, :player3 3, :player4 4, :player5 5, :player6 6}))
+             {:player1 1, :player2 2, :player3 1, :player4 2, :player5 1, :player6 2}))
