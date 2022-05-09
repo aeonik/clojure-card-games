d1535efb31336898d0ea0328ed108a56542cfede
aeonik <aeonik.chaos@gmail.com>
2022-05-09 14:10:00 -0400

    More comments thanks to Sean Cornfield.

--- src/clojure_card_games/core.clj
+++ src/clojure_card_games/core.clj
@@ -28,6 +28,11 @@
 (pp/pprint players)
 
 (comment
+  "This is the old way of doing it. Not idiomatic.
+   From Sean Cornfield: The first bit of advice I'd offer is: don't treat def like an assignment would be in other languages. 
+   First off, def always introduces a global (top-level) Var -- you don't show much of your code but you should never use def inside a function.
+   We generally think of def as introducing a single global constant.
+   Second, for repeated operations on a single value where we want to accumulate the result, take a look at -> : "
   (def game (assoc {} :game game))
   (def game (assoc-in game [:game :player1 :team] 1))
   (def game (assoc-in game [:game :player2 :team] 2))
@@ -36,7 +41,17 @@
   (def game (assoc-in game [:game :player5 :team] 1))
   (def game (assoc-in game [:game :player6 :team] 2)))
 
+(comment 
+  "Sean Cornfields way of doing it."
+  (def game-with-players
+           (-> {:game game}
+               (assoc-in [:game :player1 :team] 1)
+               (assoc-in [:game :player2 :team] 2)
+               (assoc-in [:game :player3 :team] 3)
+               ...etc...)))
+
 (def game-with-players
+  "Thanks to Sean Cornfield for this example."
   (reduce-kv (fn [data player team-number]
                (assoc-in data [:game player :team] team-number))
              {:game game}
