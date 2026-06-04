681ae06d5b709f99de77af08092699352cd7224f
aeonik <aeonik.chaos@gmail.com>
2026-06-04 16:42:18 -0400

    Split Karbosh team trick labels

--- src/clojure_card_games/karbosh/client.cljs
+++ src/clojure_card_games/karbosh/client.cljs
@@ -462,10 +462,11 @@
     [:span "Bid"]
     [:strong (bid-label (:current-bid view))]]
    [:div
-    [:span "Tricks won"]
-    [:strong {:class "trick-team-counts"}
-     [:em "Team 1: " [:b (get-in view [:tricks-this-hand 1] 0)]]
-     [:em "Team 2: " [:b (get-in view [:tricks-this-hand 2] 0)]]]]])
+    [:span "Team 1 Tricks"]
+    [:strong (get-in view [:tricks-this-hand 1] 0)]]
+   [:div
+    [:span "Team 2 Tricks"]
+    [:strong (get-in view [:tricks-this-hand 2] 0)]]])
 
 (defn table-status-html [view bid-popup]
   [:div {:class "table-status"}
