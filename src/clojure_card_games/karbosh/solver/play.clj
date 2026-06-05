cc49756db6c922c4625dd01e736fc78fdb114e85
aeonik <aeonik.chaos@gmail.com>
2026-06-05 12:22:58 -0400

    Add contract-aware Karbosh play solving

--- src/clojure_card_games/karbosh/solver/play.clj
+++ src/clojure_card_games/karbosh/solver/play.clj
@@ -48,17 +48,69 @@
                [player (player-team state player)]))
         (active-players state)))
 
-(defn state-key [state target-team]
-  [target-team
+(defn state-key [state objective]
+  [objective
    (:trump state)
    (active-players state)
    (:current-player state)
    (:current-trick state)
+   (:tricks-this-hand state)
    (teams-key state)
    (into {}
          (map (fn [player]
                 [player (hand-key state player)]))
          (active-players state))])
+
+(defn other-team [team]
+  (case team
+    1 2
+    2 1))
+
+(defn team-differential [points team]
+  (- (get points team 0)
+     (get points (other-team team) 0)))
+
+(defn contract-team [state contract]
+  (player-team state (:player contract)))
+
+(defn contract-points [state contract]
+  (rules/score-hand (:players state)
+                    contract
+                    (:tricks-this-hand state)))
+
+(defn contract-utility
+  "Return point differential from the bidding team's perspective."
+  [state contract]
+  (team-differential (contract-points state contract)
+                     (contract-team state contract)))
+
+(defn solve-value
+  "Solve a perfect-information play state with a terminal evaluator.
+
+  `max-team` chooses actions that maximize the terminal value. All other teams
+  choose actions that minimize it."
+  [state {:keys [objective max-team terminal-value]}]
+  (let [cache (atom {})]
+    (letfn [(solve* [state]
+              (let [k (state-key state objective)]
+                (if-let [cached (find @cache k)]
+                  (val cached)
+                  (let [value
+                        (if (terminal? state)
+                          (terminal-value state)
+                          (let [player (:current-player state)
+                                choose (if (= max-team (player-team state player))
+                                         max
+                                         min)
+                                scores (map (fn [card]
+                                              (solve* (play-card state player card)))
+                                            (legal-plays state))]
+                            (if (seq scores)
+                              (apply choose scores)
+                              (terminal-value state))))]
+                    (swap! cache assoc k value)
+                    value))))]
+      (solve* state))))
 
 (defn solve-future-tricks
   "Return the number of future tricks `target-team` can force from a perfect
@@ -68,30 +120,19 @@
   Duplicate physical cards are collapsed by `rules/legal-cards`, so identical
   plays do not create duplicate branches."
   [state target-team]
-  (let [cache (atom {})]
-    (letfn [(solve* [state]
-              (let [k (state-key state target-team)]
-                (if-let [cached (find @cache k)]
-                  (val cached)
-                  (let [value
-                        (if (terminal? state)
-                          0
-                          (let [player (:current-player state)
-                                choose (if (= target-team (player-team state player))
-                                         max
-                                         min)
-                                before (get-in state [:tricks-this-hand target-team] 0)
-                                scores (map (fn [card]
-                                              (let [next-state (play-card state player card)
-                                                    after (get-in next-state
-                                                                  [:tricks-this-hand target-team]
-                                                                  0)]
-                                                (+ (- after before)
-                                                   (solve* next-state))))
-                                            (legal-plays state))]
-                            (if (seq scores)
-                              (apply choose scores)
-                              0)))]
-                    (swap! cache assoc k value)
-                    value))))]
-      (solve* state))))
+  (let [baseline (get-in state [:tricks-this-hand target-team] 0)]
+    (- (solve-value state
+                    {:objective [:future-tricks target-team]
+                     :max-team target-team
+                     :terminal-value #(get-in % [:tricks-this-hand target-team] 0)})
+       baseline)))
+
+(defn solve-contract
+  "Return optimal contract point differential from the bidding team's
+  perspective."
+  [state contract]
+  (let [team (contract-team state contract)]
+    (solve-value state
+                 {:objective [:contract contract]
+                  :max-team team
+                  :terminal-value #(contract-utility % contract)})))
--- test/clojure_card_games/karbosh/solver/play_test.clj
+++ test/clojure_card_games/karbosh/solver/play_test.clj
@@ -3,7 +3,7 @@
             [clojure-card-games.karbosh.solver.play :as play]))
 
 (defn state
-  [{:keys [hands teams active-players current-player current-trick trump]}]
+  [{:keys [hands teams active-players current-player current-trick trump tricks]}]
   {:players (into {}
                   (map (fn [[player hand]]
                          [player {:hand hand
@@ -13,7 +13,7 @@
    :current-player current-player
    :current-trick (or current-trick [])
    :trump trump
-   :tricks-this-hand {1 0 2 0}})
+   :tricks-this-hand (or tricks {1 0 2 0})})
 
 (deftest solve-future-tricks-test
   (testing "solves a forced single trick"
@@ -46,3 +46,58 @@
                     :trump :♠})]
       (is (= 2 (play/solve-future-tricks s 1)))
       (is (= 0 (play/solve-future-tricks s 2))))))
+
+(deftest contract-utility-test
+  (let [s (state {:hands {:player1 [] :player2 []}
+                  :teams {:player1 1 :player2 2}
+                  :active-players [:player1 :player2]
+                  :current-player :player1
+                  :trump :♠})]
+    (testing "numeric bids score point differential from bidder team"
+      (is (= 5
+             (play/contract-utility
+               (assoc s :tricks-this-hand {1 5 2 3})
+               {:type :bid :player :player1 :bid-type :bid :value 5})))
+      (is (= -9
+             (play/contract-utility
+               (assoc s :tricks-this-hand {1 5 2 3})
+               {:type :bid :player :player1 :bid-type :bid :value 6}))))
+
+    (testing "karbosh is scored as an all-tricks contract"
+      (is (= 15
+             (play/contract-utility
+               (assoc s :tricks-this-hand {1 8 2 0})
+               {:type :bid :player :player1 :bid-type :karbosh})))
+      (is (= -16
+             (play/contract-utility
+               (assoc s :tricks-this-hand {1 7 2 1})
+               {:type :bid :player :player1 :bid-type :karbosh}))))))
+
+(deftest solve-contract-test
+  (testing "solves a forced making bid"
+    (let [s (state {:hands {:player1 [[:A :♥]]
+                            :player2 [[:K :♥]]}
+                    :teams {:player1 1 :player2 2}
+                    :active-players [:player1 :player2]
+                    :current-player :player1
+                    :trump :♠})]
+      (is (= 1
+             (play/solve-contract
+               s
+               {:type :bid :player :player1 :bid-type :bid :value 1})))
+      (is (= -2
+             (play/solve-contract
+               s
+               {:type :bid :player :player2 :bid-type :bid :value 1})))))
+
+  (testing "solves a forced failed karbosh"
+    (let [s (state {:hands {:player1 [[:K :♥]]
+                            :player2 [[:A :♥]]}
+                    :teams {:player1 1 :player2 2}
+                    :active-players [:player1 :player2]
+                    :current-player :player1
+                    :trump :♠})]
+      (is (= -16
+             (play/solve-contract
+               s
+               {:type :bid :player :player1 :bid-type :karbosh}))))))
